d4c580c30cdff630ae91f96914066219c9b68f3c
QA - CJ <qa-cj@Cccis-Mac-mini.local>
2025-10-28 10:22:30 +0800

    polish code to less flaky

--- tests/EIS_Fixtures/EIS.ts
+++ tests/EIS_Fixtures/EIS.ts
@@ -26,62 +26,84 @@ export class EIS_Login {
         await this.page.getByRole('button', { name: 'Login' }).click();
     };
     async validateLogin () {
-        await this.page.waitForResponse(response => response.url().includes('/dashboard') && response.status() === 200);
+        await expect(this.page.getByRole('heading', { name: 'All Dashboard' })).toBeVisible();
     }
 }
 
+const dashboardStore = {
+    name: '',
+    entityId: '',
+    displays: [],
+    graphCount: 0
+};
 
-let dashboardSelected: any;
-let dashboardEntityId: any;
 export class EIS_Dashboards extends EIS_Login { 
-    readonly dashboardApi: string;
     constructor(page: Page) { 
         super(page);
-        this.dashboardApi = String(process.env.DASHBOARD_API);
     }
     async get_dashboards () {
-        const dashboards =  this.page.waitForResponse(
+        const dashboards =  await this.page.waitForResponse(
             response => response.url()
             .includes('/eis/v1.0/dashboards?') 
             && response.status() === 200);
         const data = await (await dashboards).json();
         const rows = data.response.rows;
         const randomIndex = Math.floor(Math.random() * rows.length)
-        dashboardSelected = rows[randomIndex].name;
-        dashboardEntityId = rows[randomIndex].entityId;
-    }
+        dashboardStore.name = rows[randomIndex].name;
+        dashboardStore.entityId = rows[randomIndex].entityId;
+        dashboardStore.displays = rows[randomIndex].displayType;
+        let count: string | number = 0;
+        for (const display of dashboardStore.displays) { 
+            count += 1;
+        }
+        if (count > 2) { 
+            dashboardStore.graphCount = count - 2;
+            count = `${count - 2} ${dashboardStore.name}`;
+        }
+        else {
+            dashboardStore.graphCount = count;
+            count = dashboardStore.name;
+        }
+        return count;
+    }   
 
     async search_dashboard  () { 
         const dashboards = await this.get_dashboards();
         await this.page.getByRole('button', { name: 'Search... ⌘ K' }).click();
-        await this.page.getByRole('textbox', { name: 'Search dashboard...' }).pressSequentially(dashboardSelected);
-        await this.page.locator(`//span[contains(@class, "text-highlighted") and text()="${dashboardSelected}"]`).click();
-        await this.page.waitForTimeout(2_000);
+        await this.page.getByRole('textbox', { name: 'Search dashboard...' }).pressSequentially(dashboardStore.name);
+        return dashboards;
     }
 
     async select_dashboard  () {
-        this.search_dashboard();
+        const dashboards = await this.search_dashboard();
+        await this.page.getByRole('option', { name: dashboards}).click();
         await this.page.waitForResponse(
             response => response.url()
-            .includes(dashboardEntityId) 
+            .includes(dashboardStore.entityId) 
             && response.status() === 200);
     }
 
     async fullscreen_dashboard  () { 
-        this.select_dashboard();
+        await this.select_dashboard();
         await this.page.getByRole('button', { name: 'Show Full Screen' }).click();
-        // await expect(this.page.getByRole('complementary', { name: 'Sidebar Navigation' })).toBeVisible();
+        // await expect(this.page.getByRole('complementary', { name: 'Sidebar Navigation' })).toBeHidden();
         // await expect(this.page.getByText('Executive Information SystemSearch...⌘K')).toBeHidden();
         // await expect(this.page.getByText('Show Full Screen Compare')).toBeHidden();
     }
     async compare_dashboard  () {
-        this.select_dashboard();
+        await this.select_dashboard();
         await this.page.getByRole('button', { name: 'Compare' }).click();
         await this.page.getByRole('button', { name: 'Select dashboard' }).click();
-        await this.page.getByText(`+1${dashboardSelected}`).click();
+        if (dashboardStore.graphCount > 2) { 
+            await this.page.getByText(`${dashboardStore.graphCount - 2}${dashboardStore.name}`).click();
+        }
+        else {
+            await this.page.getByText(`${dashboardStore.name}`).click();
+
+        }
         await this.page.waitForResponse(
             response => response.url()
-            .includes(dashboardEntityId) 
+            .includes(dashboardStore.entityId) 
             && response.status() === 200);
     }
 
@@ -91,6 +113,6 @@ export class EIS_Dashboards extends EIS_Login {
             .includes('recents/dashboards?') 
             && res.status() === 200);
         const data = await (await viewedDashboards).json();
-        expect(data.response.rows[0].name).toEqual(dashboardSelected);
+        expect(data.response.rows[0].name).toEqual(dashboardStore.name);
     }
 }
--- tests/EIS_Specs/EIS.spec.ts
+++ tests/EIS_Specs/EIS.spec.ts
@@ -1,4 +1,4 @@
-import {test} from '@playwright/test';
+import {test, expect} from '@playwright/test';
 import { EIS_Login, EIS_Dashboards } from '../EIS_Fixtures/EIS';
 
 test.describe('EIS Portal Login', () => {
@@ -13,25 +13,36 @@ test.describe('EIS Portal Login', () => {
     });
     test('TS-EIS-100', async ({ page }) => {
         await loginPage.validateLogin();
+        console.log(`'\x1b[32m%s\x1b[0m'`, 'Login Successful');
     });
     test('TS-EIS-101', async ({ page }) => {
         await dashboardPage.select_dashboard();
+        console.log(`'\x1b[32m%s\x1b[0m'`, 'Viewed Dashboard Successfully');
+
     });
     test('TS-EIS-105', async ({ page }) => {
         await dashboardPage.compare_dashboard();
+        console.log(`'\x1b[32m%s\x1b[0m'`, 'Compared Dashboard Successfully');
+
     });
     test('TS-EIS-106', async ({ page }) => {
-        await dashboardPage.fullscreen_dashboard();
+        await dashboardPage.fullscreen_dashboard(); 
+        console.log(`'\x1b[32m%s\x1b[0m'`, 'Fullscreen Dashboard Successfully');
+
     });
     test('TS-EIS-108', async ({ page }) => {
         await dashboardPage.search_dashboard();
+        console.log(`'\x1b[32m%s\x1b[0m'`, 'Searched Dashboard Successfully');
+
     });
     test('TS-EIS-112', async ({ page }) => {
-        await dashboardPage.search_dashboard();
+        await dashboardPage.select_dashboard();
         await page.getByRole('main').getByText('Executive Information System').click();
-        await page.waitForTimeout(2_000);
+        await expect(page.getByRole('heading', { name: 'All Dashboard' })).toBeVisible();
         await page.reload();
         await dashboardPage.viewed_dashboards();
+        console.log(`'\x1b[32m%s\x1b[0m'`, 'Recently Viewed Dashboard Successfully');
+
     });
 });
     
